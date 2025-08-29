58e36a4075c56f5f0a10873e9922c592368eef39
SycamoreQ <kaushik.80405@gmail.com>
2025-08-29 22:15:43 +0530

    added files

--- Graph/query/context/entity_extractor.py
+++ Graph/query/context/entity_extractor.py
@@ -11,7 +11,8 @@ from Graph.retrieval.entity import (
 )
 from enum import Enum
 
-from vectorstore import BaseVectorStore
+
+from vectorstore.base import VectorStoreSearchResult , VectorStoreDocument , BaseVectorStore
 from typing import List , Dict , Any
 from Graph.data.entity import Entity
 from Graph.data.relationship import Relationship
@@ -30,12 +31,49 @@ class EntityVectorStoreKey(str , Enum):
 
         msg = f"Invalid EntityVectorStoreKey: {value}"
         raise ValueError(msg)
+    
+
+    def map_query_to_entity(
+            query: str , 
+            text_embedding_vectorstore: BaseVectorStore,
+            embedding_vectorstore_ids: str =  EntityVectorStoreKey.ID,
+            text_embedder =  EmbeddingModel
+    ) -> list[Entity]: 
+        
+        if query != " " : 
+            
+            search_result = BaseVectorStore.similarity_search_by_subgraph(
+                text = query , 
+                text_embedder= lambda t : text_embedder.embed(t)
+            )
+    
+
+
+        
 
     
     def find_entities_using_subgraph(
             entity_name : str ,
             all_entities: list[Entity],
-            all_relationships : list[Relationship],            
+            all_relationships : list[Relationship],
+            exclude_entity_names: list[str] | None = None,            
     ) -> list[Entity]:
+        if exclude_entity_names is None:
+            exclude_entity_names = []
+        
+        entity_relatiosnships = [
+            rel 
+            for rel in all_relationships
+            if rel.source == entity_name or rel.target == entity_name  
+        ]
+
+        source_enttity_subgraph = {rel.source for rel in entity_relatiosnships}
+        target_entity_subgraph = {rel.target for rel in entity_relatiosnships}
+
+        #find the similarity between the source and target entities 
+        
+        
+    
+        
         
     
