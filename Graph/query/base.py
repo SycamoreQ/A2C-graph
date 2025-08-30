76f770a70e0ee9d4d3d6f6bc0658e36d57db44b0
SycamoreQ <kaushik.80405@gmail.com>
2025-08-30 22:23:48 +0530

    added query bases

--- Graph/query/base.py
+++ Graph/query/base.py
@@ -24,7 +24,8 @@ class AuthorQuery:
                  token_encoder: tiktoken.Encoding,
                  model_params : dict[str, Any] = None | None,
                  context_size : int = 2048 , 
-                 context_params : dict[str, Any] = None | None
+                 context_params : dict[str, Any] = None | None,
+                 entity_type: str = "author"
                  ):
         self.model = model
         self.token_encoder = token_encoder
@@ -42,3 +43,21 @@ class AuthorQuery:
     ) -> QuestionResult:
         "Generate a response "
     
+
+
+class PubicationQuery: 
+    
+    def __init__(self,
+                 model : ChatModel,
+                 token_encoder: tiktoken.Encoding,
+                 model_params: dict[str , Any] = None | None,
+                 context_size: int = 2048,
+                 context_params: dict[str , Any] = None | None ,
+                 entity_type : str = "publication"
+                   ):
+        self.model = model 
+        self.token_encoder = token_encoder
+        self.model_params= model_params if model_params is not None else {}
+        self.context_size = context_size
+        self.context_params = context_params if context_params is not None else {}
+        
